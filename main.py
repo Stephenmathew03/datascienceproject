bcdd8eb026462821e625cf83dbc64a1cbd682a75
Stephen Mathew~ <stephenmathew5703@gmail.com>
2025-08-06 18:19:44 +0530

    Data Transformation update

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from src.datascience.pipeline.data_ingestion_pipeline import (
 from src.datascience.pipeline.data_validation_pipeline import (
     DataValidationTrainingPipeline,
 )
+from src.datascience.pipeline.data_transformation_pipeline import (
+    DataTransformationTrainingPipeline,
+)
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -23,6 +26,17 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataValidationTrainingPipeline()
     data_ingestion.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataTransformationTrainingPipeline()
+    data_ingestion.initiate_data_transformation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
